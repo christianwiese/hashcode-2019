e1358f9e218d063ab9eb9275982927e458703610
christianwiese <christian.wiese@freiheit.com>
2019-02-28 18:45:16 +0100

    initial commit

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/christianwiese/hashcode-2019
+
+go 1.12
---
+++ hashcode/dumpOutput.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Output []Command
+
+type Command struct {
+	droneID     int
+	operation   string
+	warhouseID  int
+	orderID     int
+	productID   int
+	numProducts int
+}
+
+func Dump(out Output, file string) {
+	output := fmt.Sprintf("./%s.out", strings.TrimSuffix(file, ".in"))
+	f, _ := os.Create(output)
+	defer f.Close()
+
+	//w := bufio.NewWriter(f)
+	//
+	//num := len(out)
+	//w.WriteString(fmt.Sprintf("%d", num))
+	//w.WriteString("\n")
+	//for i, c := range out {
+	//	if c.operation == "D" {
+	//		w.WriteString(fmt.Sprintf("%d %s %d %d %d", c.droneID, c.operation, c.orderID, c.productID, c.numProducts))
+	//	} else {
+	//		w.WriteString(fmt.Sprintf("%d %s %d %d %d", c.droneID, c.operation, c.warhouseID, c.productID, c.numProducts))
+	//	}
+	//
+	//	if i < num {
+	//		w.WriteString("\n")
+	//	}
+	//}
+	//w.Flush()
+}
